94b0848216fa6a606452d4a3019e05fb081f4e73
Vaishnavigupta97 <vaishnavigupta6389@gmail.com>
2023-05-11 19:21:40 +0530

    add delete functionlity

--- objLocalStorage.js
+++ objLocalStorage.js
@@ -8,11 +8,33 @@ let number = document.getElementById("number").value;
 const parentEle = document.getElementById("unOrderList");
 let li = document.createElement("li");
 li.innerHTML = li.innerHTML + `name : ${name}, email : ${email}, PhoneNumber : ${number}`
+li.style.backgroundColor = "grey";
+li.style.fontSize = "20px";
+li.style.padding = "10px";
 parentEle.appendChild(li);
+let allObj = [];
 let obj = {
     name,
     email, 
     number
 }
-localStorage.setItem("addobj", JSON.stringify(obj));
+allObj.push(obj);
+localStorage.setItem("addobj", JSON.stringify([...allObj]));
+// let abc = JSON.parse(localStorage.getItem("addobj"));
+// console.log(abc);
+
+// create a delete button 
+let deleteBtn = document.createElement("button");
+deleteBtn.textContent = "Delete"
+deleteBtn.style.backgroundColor = "red";
+deleteBtn.style.padding = "2px";
+deleteBtn.style.border = "none";
+deleteBtn.style.fontSize = "20px";
+deleteBtn.style.marginLeft = "10px";
+li.appendChild(deleteBtn);
+deleteBtn.addEventListener("click", deleteFunc);
+function deleteFunc(){
+   deleteBtn.parentElement.remove();
+   localStorage.removeItem(addobj);
+}
 }
